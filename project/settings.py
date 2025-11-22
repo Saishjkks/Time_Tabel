04d863bd359a20d47b651a6367ca4beb68e8dc0e
Saish Janaga <115801098+Saishjkks@users.noreply.github.com>
2025-11-22 11:30:29 +0530

    Configure media file settings in settings.py
    Added media file settings for Django project.

--- project/settings.py
+++ project/settings.py
@@ -127,7 +127,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -136,6 +135,24 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # In settings.py
 
 AUTH_USER_MODEL = 'app.Instructor'
+
+# =============================
+# STATIC & MEDIA FILE SETTINGS
+# =============================
+
+STATIC_URL = '/static/'
+
+# Folder where Django collects all static files during deployment
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Folder where your custom static files are located (optional but recommended)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 
 
  
@@ -321,4 +338,5 @@ JAZZMIN_UI_TWEAKS = {
 STATIC_URL = 'static/'
 import os
 MEDIA_URL="/media/"
+
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
